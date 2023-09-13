2970ff2ecedd0940d2972f08b171a205598b9de2
ArShuranov <arshuranov@yandex.ru>
2023-09-13 17:02:06 +0700

    Initial commit add greetings in telegram bot "/start" connected DB postgre

---
+++ src/main/java/ru/arshuranov/PlannerBot/PlannerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.arshuranov.PlannerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/arshuranov/PlannerBot/PlannerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arshuranov.PlannerBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
